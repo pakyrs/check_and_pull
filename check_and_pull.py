84f30424344cd959ba7689f2ff167d5cc8553684
pacadmin <pakyrs@gmail.com>
2023-06-29 10:43:13 +0100

    Commit from zoidberg

---
+++ check_and_pull.py
@@ -0,0 +1,29 @@
+import os
+import subprocess
+
+repo_path = "/home/pacadmin/dotfiles"
+repo_url = "git@github.com:pakyrs/dotfiles.git"
+branch = "master"
+
+# Go to the repository directory
+os.chdir(repo_path)
+
+# Check if there are any changes on the remote branch
+try:
+    subprocess.check_output(["git", "fetch", "origin", branch])
+    output = subprocess.check_output(["git", "rev-list", "HEAD..origin/" + branch, "--count"])
+    changes = int(output.strip())
+    
+    if changes > 0:
+        # There are changes on the remote branch, perform a pull
+        subprocess.call(["git", "pull", "origin", branch])
+        # Update stow links
+        command = "stow -Rv */"
+        subprocess.run(command, shell=True, check=True)
+    else:
+        # Repository is up to date
+        print("Repository is up to date. No changes to pull.")
+except subprocess.CalledProcessError as e:
+    # Failed to fetch or pull from the remote repository
+    print("Failed to fetch or pull from the remote repository. Error:", e)
+
